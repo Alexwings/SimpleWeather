4d04d55e033b94e57ac4705e866e0ad5087029d1
Xinyuan Wang <alexwang@FightUp.local>
2016-12-13 00:56:21 -0600

    create SimpleWeather project

---
+++ SimpleWeather/Constant.h
@@ -0,0 +1,33 @@
+//
+//  Constant.h
+//  SimpleWeather
+//
+//  Created by Xinyuan Wang on 12/12/16.
+//  Copyright © 2016 RJT. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+
+#endif /* Constant_h */
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSString *const iconKey;
+FOUNDATION_EXPORT NSString *const timeKey;
+FOUNDATION_EXPORT NSString *const summaryKey;
+FOUNDATION_EXPORT NSString *const sunriseKey;
+FOUNDATION_EXPORT NSString *const sunsetKey;
+FOUNDATION_EXPORT NSString *const tempMinKey;
+FOUNDATION_EXPORT NSString *const tempMaxKey;
+FOUNDATION_EXPORT NSString *const visibilityKey;
+FOUNDATION_EXPORT NSString *const humidityKey;
+FOUNDATION_EXPORT NSString *const currentTempKey;
+FOUNDATION_EXPORT NSString *const windspeedKey;
+FOUNDATION_EXPORT NSString *const probabilityKey;
+
+FOUNDATION_EXPORT NSString *const currentInfoKey;
+FOUNDATION_EXPORT NSString *const dailyInfoKey;
+FOUNDATION_EXPORT NSString *const hourlyInfoKey;
+FOUNDATION_EXPORT NSString *const dataInfoKey;
---
+++ SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/SimpleWeather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Xinyuan Wang on 12/12/16.
+//  Copyright © 2016 RJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
